dc4499bd7dd4c5fd9b5e35ebaf00f8b5293daf37
leesangha <sanghalee1@naver.com>
2023-01-31 11:49:02 +0900

    prismic initial settings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { PrismicPreview } from "@prismicio/next";
+import { PrismicProvider } from "@prismicio/react";
+import type { AppProps } from "next/app";
+import { repositoryName } from "../prismicio";
+import Link from "next/link";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <PrismicProvider internalLinkComponent={(props) => <Link {...props} />}>
+      <PrismicPreview repositoryName={repositoryName}>
+        <Component {...pageProps} />
+      </PrismicPreview>
+    </PrismicProvider>
+  );
 }
