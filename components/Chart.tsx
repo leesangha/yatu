e6f1c8ccebcb036a88a514ccce8e72b5a4b8ffbb
leesangha <sanghalee1@naver.com>
2023-03-20 21:54:55 +0900

    차트 custom

--- components/Chart.tsx
+++ components/Chart.tsx
@@ -39,23 +39,23 @@ export default function CandleChart(props: IProps) {
       height: 300,
     });
 
-    const areaSeries = chart.addAreaSeries({
-      lineColor: "#2962FF",
-      topColor: "#2962FF",
-      bottomColor: "rgba(41, 98, 255, 0.28)",
-    });
-    areaSeries.setData([
-      { time: "2018-12-22", value: 32.51 },
-      { time: "2018-12-23", value: 31.11 },
-      { time: "2018-12-24", value: 27.02 },
-      { time: "2018-12-25", value: 27.32 },
-      { time: "2018-12-26", value: 25.17 },
-      { time: "2018-12-27", value: 28.89 },
-      { time: "2018-12-28", value: 25.46 },
-      { time: "2018-12-29", value: 23.92 },
-      { time: "2018-12-30", value: 22.68 },
-      { time: "2018-12-31", value: 22.67 },
-    ]);
+    // const areaSeries = chart.addAreaSeries({
+    //   lineColor: "#2962FF",
+    //   topColor: "#2962FF",
+    //   bottomColor: "rgba(41, 98, 255, 0.28)",
+    // });
+    // areaSeries.setData([
+    //   { time: "2018-12-22", value: 32.51 },
+    //   { time: "2018-12-23", value: 31.11 },
+    //   { time: "2018-12-24", value: 27.02 },
+    //   { time: "2018-12-25", value: 27.32 },
+    //   { time: "2018-12-26", value: 25.17 },
+    //   { time: "2018-12-27", value: 28.89 },
+    //   { time: "2018-12-28", value: 25.46 },
+    //   { time: "2018-12-29", value: 23.92 },
+    //   { time: "2018-12-30", value: 22.68 },
+    //   { time: "2018-12-31", value: 22.67 },
+    // ]);
 
     const candlestickSeries = chart.addCandlestickSeries({
       upColor: "#26a69a",
